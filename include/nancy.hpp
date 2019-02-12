e3c164c4560877181544ee4ef2ca3509e285b148
Chen <langchen@uab.edu>
2019-02-12 09:33:49 -0600

    v1

--- include/nancy.hpp
+++ include/nancy.hpp
@@ -15,9 +15,10 @@ const json userData = {
     {
         "自动赛",
         {
+            {"红方&蓝方", false},
             {"前场&后场", false},
             {"高旗&中旗", false},
-            {"不开台&开台", false},
+            {"不开台&开台",false},
         },
     },
     {
